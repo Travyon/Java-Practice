df219813180b0fa8fed7a3c33cfc78a32d7bfc02
Travyon <treystvns05@gmail.com>
2021-12-07 22:50:19 -0600

    Java practice

---
+++ String-Methods/length.java
@@ -0,0 +1,20 @@
+/**
+ * The length method gives returns the length
+ * or total number of characters, of the string 
+ * it's called on.
+ * 
+ * Expect spaces and punctuation
+ * to be included in the count.
+ */
+
+public class length {
+    public static void main(String[] args){
+        String str1 = "Travyon Stevens";
+        String str2 = "Takari Coulter";
+
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+
+    }
+    
+}
