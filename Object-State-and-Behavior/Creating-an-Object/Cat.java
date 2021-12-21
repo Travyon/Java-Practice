cfe15677e5fdabf87116787bda6204a0e0c9ad57
Travyon <treystvns05@gmail.com>
2021-12-20 22:51:49 -0600

    PracticedObjects12202021

---
+++ Object-State-and-Behavior/Creating-an-Object/Cat.java
@@ -0,0 +1,38 @@
+/**
+ * Objects and classes are closely related
+ * because classes define attributes and behaviors
+ * of an object. 
+ * 
+ * To create an object:
+ * declare a variable in the 'main()' method 
+ * with the class name as the data type and set
+ * 
+ * 
+ * Ex: Animal dog;
+ * 
+ * and set the value to a call to the class' constructor
+ * 
+ * Ex: new Animal();
+ */
+
+ /**
+  * The class name is used as a data type because objects
+  * have reference data types. 
+  * 
+  * This means that the value of the variable
+  * a reference to an object's(instance's) memory address.
+  */
+
+
+class Cat {
+    // Class constructor
+    public Cat(){
+        // Instructions for creating an instance of Cat gpoes here:
+    }
+
+    public static void main(String[] args){
+        // Create a Cat object by calling the constructor:
+        Cat mere = new Cat();
+    }
+    
+}
